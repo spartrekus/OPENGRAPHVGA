2a43668fd964cdf9acdcfb8622ce5186649d5aec
spartrekus <spartrekus@gmail.com>
2018-01-07 21:26:28 +0100

    Add files via upload

---
+++ 10.setpixel_video_driver.c
@@ -0,0 +1,37 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "opengraphvga.c" 
+
+void demo_put_pixel(void)
+{
+  int px; int py;
+  int couleur;
+  int n; 
+  px = 10 ; py = 10; 
+  couleur = 10;
+  for ( n = 0; n< 100; n++)
+  {
+     vga_put_pixel( px, py );
+     px++; py++;
+     couleur++;
+  }
+}
+
+
+int main(void)
+{
+   def_vga();  // init screen size
+   def_vga_video_driver( "sdl" );  // option: init the display driver for BSD "sdl" 
+   init_vga(); // init screen vga
+
+   vga_cls_screen( 0 );
+   demo_put_pixel();
+   vga_getch();
+   close_vga(); // close clean
+   return 0;
+}
+
+

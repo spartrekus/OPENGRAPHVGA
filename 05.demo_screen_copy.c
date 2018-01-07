789498d87d791995ed5d8eca365ab63207dbe924
spartrekus <spartrekus@gmail.com>
2018-01-07 20:25:09 +0100

    Add files via upload

---
+++ 05.demo_screen_copy.c
@@ -0,0 +1,44 @@
+
+
+
+// fill screen with a color
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "opengraphvga.c" 
+
+void remplissage_ecran(void)
+{
+  int px; int py;
+  int couleur;
+  int n; 
+  px = 3 ; py = 10; 
+  for ( couleur = 1; n< 29 ; couleur++)
+  {
+     rectangle( px, py, 40, 40, couleur );
+     px+=10; py+=5;
+  }
+}
+
+void demo_move_image(void)
+{
+   cls_ecran( 3 ); 
+   remplissage_ecran();
+   getch();
+
+   // y1 x1 y2 x2 ==> y3 x3
+   copy_image( 10, 10, 100, 70, 150, 50 );
+   getch();
+}
+
+
+int main(void)
+{
+   init_vga(); // init screen vga
+   demo_move_image();
+   fermeture_vga(); // close clean
+   return 0;
+}
+
+

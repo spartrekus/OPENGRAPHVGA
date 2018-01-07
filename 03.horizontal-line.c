aaa08db2322c8c171dde47d468b88590ca2e534a
spartrekus <spartrekus@gmail.com>
2018-01-07 20:16:19 +0100

    Add files via upload

---
+++ 03.horizontal-line.c
@@ -0,0 +1,31 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "graphvga.c" 
+
+void remplissage_ecran(void)
+{
+  int px; int py;
+  int couleur;
+  int n; 
+  px = 0 ; py = 0; 
+  for ( couleur = 0; couleur< 200; couleur++)
+  {
+    h_line( px, py, 320, couleur );
+    py++;
+  }
+}
+
+
+int main(void)
+{
+   init_vga(); // init screen vga
+   remplissage_ecran();
+   getch();
+   fermeture_vga(); // close clean
+   return 0;
+}
+
+
